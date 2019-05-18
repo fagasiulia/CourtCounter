14d12344d568f53083b27cccb3f5694eeed94ff4
fagas <fagas.iulia@yahoo.com>
2019-05-18 09:11:02 +0300

    Interface created

---
+++ app/src/main/java/com/example/courtcounter/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.courtcounter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    int scoreTeamA = 0;
+    int soreTeamB = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * Displays the given score for Team A.
+     */
+    public void displayForTeamA(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_a_score);
+        scoreView.setText(String.valueOf(score));
+    }
+}
